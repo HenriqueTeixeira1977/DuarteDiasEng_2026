4b62d31054194680ba7fa1afd193df114ab83104
Henrique Teixeira <henriqueteixeira.wd@gmail.com>
2026-01-02 00:48:51 -0300

    Update260102004938

--- duartedias_schema.sql
+++ duartedias_schema.sql
@@ -1,6 +1,6 @@
 -- duartedias_schema.sql
-CREATE DATABASE IF NOT EXISTS duartediasengenharia2026 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-USE duartediasengenharia2026;
+CREATE DATABASE IF NOT EXISTS u588089239_duartediaseng CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE u588089239_duartediaseng;
 
 -- tabela de usuários/admins
 CREATE TABLE IF NOT EXISTS admins (
